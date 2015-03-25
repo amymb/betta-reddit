3a430c18365e08649f1a2dc0ab5aff15c8886603
Amy Bertken <amy.bertken@gmail.com>
2015-03-25 14:40:00 -0600

    pass tests for show, edit, and update post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,22 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
 
   private
 
--- spec/feature/posts_spec.rb
+++ spec/feature/posts_spec.rb
@@ -31,5 +31,25 @@ feature 'Posts CRUD' do
     expect(page).to have_content("Content is content for post title")
   end
 
+  scenario 'signed in user can update post' do
+    post = create_post
+    sign_in_user
+
+    expect(current_path).to eq root_path
+
+    click_on "Best Post"
+    expect(page).to have_content("Best Post")
+    click_on "Edit"
+    fill_in "Title", with: "Better Post"
+    fill_in "Content", with: "This is an update"
+
+    click_on "Update Post"
+
+    expect(current_path).to eq post_path(post)
+    expect(page).to have_content("Better Post")
+    expect(page).to have_content("This is an update")
+  end
+
+#  scenario 'signed in user can delete post' do
 
 end
